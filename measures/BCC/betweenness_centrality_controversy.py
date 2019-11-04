88da47cc8567f3e6a2ea8963a74ed4f22ccb0810
PatrickRi <PatrickRi@users.noreply.github.com>
2019-11-04 16:26:58 +0100

    Update betweenness_centrality_controversy.py

--- measures/BCC/betweenness_centrality_controversy.py
+++ measures/BCC/betweenness_centrality_controversy.py
@@ -18,23 +18,22 @@ class BCC(Measure):
 
     def calculate(self) -> float:
         self.logger.info('Retrieve centralities')
-        dict_edge_betweenness = get_centralities(self.graph, self.dataset)
-
-        self.logger.info('Split lists')
-        eb_list = []
-        for left_node in self.left_part:
-            for right_node in self.right_part:
-                if self.graph.has_edge(left_node, right_node):
-                    if (left_node, right_node) in dict_edge_betweenness:
-                        edge_betweenness = dict_edge_betweenness[(left_node, right_node)]
-                        eb_list.append(edge_betweenness)
-                    else:
-                        edge_betweenness = dict_edge_betweenness[(right_node, left_node)]
-                        eb_list.append(edge_betweenness)
-        eb_list_all = [x for x in dict_edge_betweenness.values()]
+        eb_list_all, eb_list = get_centralities(self.graph, self.dataset, self.left_part, self.right_part)
+        eb_list_all = self.replace_with_small(eb_list_all)
+        eb_list = self.replace_with_small(eb_list)
         self.logger.info('Calculate entropy')
         entr = entropy(self.sample_from_kde(np.array(eb_list)), self.sample_from_kde(np.array(eb_list_all)))
-        return 1 - np.exp(-1.0 * entr)
+        return 1 - np.exp(-1.0 * entr)[0]
+
+
+    def replace_with_small(self, arr):
+        result = []
+        for x in arr:
+            if x == 0:
+                result.append(0.000001)
+            else:
+                result.append(x)
+        return result
 
     def sample_from_kde(self, values: np.ndarray):
         kde_fitted = KernelDensity(bandwidth=self.bandwidth).fit(values.reshape(-1, 1))
