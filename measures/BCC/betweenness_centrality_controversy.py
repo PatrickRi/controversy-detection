f6134286d374070b3f1013e552e06d15bce1930e
Patrick <pat.riemer@gmail.com>
2019-11-12 15:40:08 +0100

    fix wrong formatting (error at constructor...)

--- measures/BCC/betweenness_centrality_controversy.py
+++ measures/BCC/betweenness_centrality_controversy.py
@@ -19,38 +19,35 @@ class BCC(Measure):
         self.left_dict = list_to_dict(self.left_part)
         self.right_dict = list_to_dict(self.right_part)
 
+    def calculate(self) -> float:
+        self.logger.info('Retrieve centralities')
+        dict_edge_betweenness = get_centralities(self.graph, self.dataset, self.cache)
 
-def calculate(self) -> float:
-    self.logger.info('Retrieve centralities')
-    dict_edge_betweenness = get_centralities(self.graph, self.dataset, self.cache)
+        self.logger.info('Split lists')
+        eb_list = []
+        for s, t in self.graph.edges:
+            if (s in self.left_part and t in self.right_part) or (s in self.right_part and t in self.left_part):
+                if (s, t) in dict_edge_betweenness:
+                    edge_betweenness = dict_edge_betweenness[(s, t)]
+                    eb_list.append(edge_betweenness)
+                else:
+                    edge_betweenness = dict_edge_betweenness[(t, s)]
+                    eb_list.append(edge_betweenness)
+        eb_list_all = self.replace_with_small(list(dict_edge_betweenness.values()))
+        eb_list = self.replace_with_small(eb_list)
+        self.logger.info('Calculate entropy')
+        entr = entropy(self.sample_from_kde(np.array(eb_list)), self.sample_from_kde(np.array(eb_list_all)))
+        return 1 - np.exp(-1.0 * entr)[0]
 
-    self.logger.info('Split lists')
-    eb_list = []
-    for s, t in self.graph.edges:
-        if (s in self.left_part and t in self.right_part) or (s in self.right_part and t in self.left_part):
-            if (s, t) in dict_edge_betweenness:
-                edge_betweenness = dict_edge_betweenness[(s, t)]
-                eb_list.append(edge_betweenness)
+    def replace_with_small(self, arr):
+        result = []
+        for x in arr:
+            if x < 0.000001:
+                result.append(0.000001)
             else:
-                edge_betweenness = dict_edge_betweenness[(t, s)]
-                eb_list.append(edge_betweenness)
-    eb_list_all = self.replace_with_small(list(dict_edge_betweenness.values()))
-    eb_list = self.replace_with_small(eb_list)
-    self.logger.info('Calculate entropy')
-    entr = entropy(self.sample_from_kde(np.array(eb_list)), self.sample_from_kde(np.array(eb_list_all)))
-    return 1 - np.exp(-1.0 * entr)[0]
+                result.append(x)
+        return result
 
-
-def replace_with_small(self, arr):
-    result = []
-    for x in arr:
-        if x < 0.000001:
-            result.append(0.000001)
-        else:
-            result.append(x)
-    return result
-
-
-def sample_from_kde(self, values: np.ndarray):
-    kde_fitted = KernelDensity(bandwidth=self.bandwidth).fit(values.reshape(-1, 1))
-    return kde_fitted.sample(10000)
+    def sample_from_kde(self, values: np.ndarray):
+        kde_fitted = KernelDensity(bandwidth=self.bandwidth).fit(values.reshape(-1, 1))
+        return kde_fitted.sample(10000)
