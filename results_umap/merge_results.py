16b7c3e87e39217ad1e45d0c46f4b8e011b68bd8
Patrick <pat.riemer@gmail.com>
2019-11-29 18:35:29 +0100

    update results

--- results_umap/merge_results.py
+++ results_umap/merge_results.py
@@ -8,4 +8,4 @@ for file in glob.glob('output*.csv'):
     df_merged = df_merged.append(df)
 df_merged = df_merged[['dataset', 'measure', 'result', 'duration']]
 df_merged.index.name = 'idx'
-df_merged.to_csv('merged_results_gc.csv')
+df_merged.to_csv('merged_results_umap.csv')
